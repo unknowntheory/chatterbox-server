39d77ffd864cc2491eb4e5912ae8c42f35724a6f
Hack Reactor Student <student@hackreactor.com>
2017-10-30 19:42:21 -0700

    Pass all tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,88 +1,3 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
-var path = require('path');
-
-
-
-
-var messages = [];
-
-
-
-
-
-
-
-var requestHandler = function(request, response) {
-
-  console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  if (request.url === '/chatterbox/classes/message') {
-
-    if (request.method === 'GET') {
-      var statusCode = 200;
-      var headers = defaultCorsHeaders;  
-      headers['Content-Type'] = 'application/json';
-      response.writeHead(statusCode, headers);
-// 'Here are the messages.', 
-      response.end(JSON.stringify(messages));
-
-    } else if (request.method === 'POST') {
-
-      let body = [];
-      request.on('data', (chunk) => {
-        body.push(chunk);
-      }).on('end', () => {
-        // console.log(JSON.stringify(body));
-        body = Buffer.concat(body).toString();
-        // console.log(body);
-        messages.push(JSON.parse(body));
-        // console.log(messages);
-      });
-
-
-      var statusCode = 201;
-      var headers = defaultCorsHeaders;
-      headers['Content-Type'] = 'text/plain';
-      response.writeHead(statusCode, headers);
-      response.end('Message posted!');
-    } else if (request.method === 'OPTIONS') {
-      var statusCode = 200;
-      var headers = defaultCorsHeaders;
-      headers['Content-Type'] = 'text/plain';
-      response.writeHead(statusCode, headers);
-      response.end('Allowed methods: POST, GET.');
-    } else {
-      var statusCode = 404;
-      var headers = defaultCorsHeaders;
-      headers['Content-Type'] = 'text/plain';
-      response.writeHead(statusCode, headers);
-      response.end('Not allowed!');
-    }
-  } else {
-    var statusCode = 404;
-    var headers = defaultCorsHeaders;
-    headers['Content-Type'] = 'text/plain';
-    response.writeHead(statusCode, headers);
-    response.end('Not allowed!');
-  }
-
-};
-
-exports.requestHandler = requestHandler;
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -97,5 +12,66 @@ var defaultCorsHeaders = {
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
+}; 
+
+var path = require('path');
+
+var messages = [];
+var rooms = [];
+
+var requestHandler = function(request, response) {
+
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
+
+  // 404 if wrong url
+  if (request.url !== '/classes/messages') {
+    var statusCode = 404;
+    var headers = defaultCorsHeaders;
+    headers['Content-Type'] = 'text/plain';
+    response.writeHead(statusCode, headers);
+    response.end('Not allowed!');
+  } else {
+
+    // deal with POST request
+    if (request.method === 'POST') {
+      let body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        messages.push(JSON.parse(body));
+        var statusCode = 201;
+        var headers = defaultCorsHeaders;
+        headers['Content-Type'] = 'application/json';
+        response.writeHead(statusCode, headers);
+        response.end(JSON.stringify({results: messages}));
+      });
+
+    // deal with GET request
+    } else if (request.method === 'GET') {
+      var statusCode = 200;
+      var headers = defaultCorsHeaders;  
+      headers['Content-Type'] = 'application/json';
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify({results: messages}));
+
+    // deal with OPTIONS request
+    } else if (request.method === 'OPTIONS') {
+      var statusCode = 200;
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = 'text/plain';
+      response.writeHead(statusCode, headers);
+      response.end('Allowed methods: POST, GET.');
+
+    // 404 if other type of request
+    } else {
+      var statusCode = 404;
+      var headers = defaultCorsHeaders;
+      headers['Content-Type'] = 'text/plain';
+      response.writeHead(statusCode, headers);
+      response.end('Not allowed!');
+    }
+  }
 };
 
+exports.requestHandler = requestHandler;
